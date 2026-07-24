a839494007ee3981f827706e6822c9a430d6d924
Akshat Singh <vs22022005@gmail.com>
2026-07-24 20:33:49 +0530

    Add All Lengths Subtraction [A] from Codeforces

---
+++ 2143/A - All Lengths Subtraction/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+ 
+        int isInc = 1;
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i+1] < arr[i]){
+                isInc = 0;
+                continue;
+            }
+            if(arr[i+1] > arr[i] && isInc != 0){
+                isInc = 1;
+            }
+            else{
+                isInc = -1;
+                break;
+            }
+        }
+ 
+        if(isInc == 0 || isInc == 1){
+            cout << "YES" << endl;
+        }
+        else if(isInc == -1){
+            cout << "NO" << endl;
+        }
+    }
+ 
+    return 0;
+}
